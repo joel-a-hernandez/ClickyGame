9109b479c212124e0abb04a9de0e1e2d705fc11c
Joel Hernandez <joel24hernan@gmail.com>
2019-07-30 22:21:50 -0500

    Updates

---
+++ precache-manifest.5e1398476da64db4cfe09d49e026f8a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e2bf810024972dd556e58591c69017",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "927cf2adea5bfc68b8ea",
+    "url": "/ClickyGame/static/css/main.594544f1.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ClickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "927cf2adea5bfc68b8ea",
+    "url": "/ClickyGame/static/js/main.17999f82.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ClickyGame/precache-manifest.77673aa42e34bb54ba1b8fd2f411ec0b.js"
+  "/ClickyGame/precache-manifest.5e1398476da64db4cfe09d49e026f8a3.js"
 );
 
 self.addEventListener('message', (event) => {
